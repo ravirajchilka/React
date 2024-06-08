1fe8634bb03054e81188879974ebcce6656e6135
Raviraj <ravirajchilka10@gmail.com>
2024-06-08 18:09:15 -0400

    added calendar and time selector

--- Projects/To_Do_List/src/LeftSide/ItemList/ItemList.jsx
+++ Projects/To_Do_List/src/LeftSide/ItemList/ItemList.jsx
@@ -3,8 +3,16 @@ import "../ItemList/ItemList.css";
 function ItemList(props) {
   let item = props.itemValue.map((item, _) => {
     return (
+      
       <div className="mt10 gridCl" key={Math.random()}>
-        <div>{item}</div>
+       
+        <div className="dispFlex">
+          <div className="mr10 width100cl">{item}</div>
+          <input className="mr10 width100cl" type="date" id="birthday" name="birthday"></input>
+          <input className="width100cl" type="time" id="appt" name="appt"></input>
+         <button className="borderCl">Delete</button>
+        </div>
+     
       </div>
     );
   });
