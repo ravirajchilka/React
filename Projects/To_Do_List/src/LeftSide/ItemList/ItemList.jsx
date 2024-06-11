475485e7656ab0971f9ce060a7a0a8191984daaf
Raviraj <ravirajchilka10@gmail.com>
2024-06-11 18:37:18 -0400

    added more functionalities

--- Projects/To_Do_List/src/LeftSide/ItemList/ItemList.jsx
+++ Projects/To_Do_List/src/LeftSide/ItemList/ItemList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useRef,useState } from 'react';
+import React, { useContext,useRef,useState,useEffect } from 'react';
 import Button from '@mui/material/Button';
 import "../ItemList/ItemList.css";
 import { MyContext } from "../../MyProvider"
@@ -8,30 +8,46 @@ function ItemList(props) {
 
   const { setData } = useContext(MyContext);
 
-  const [selectedDateAndTime, setSelectedDateAndTime] = useState({date:"",time:""});
+  const [selectedDateAndTime, setSelectedDateAndTime] = useState(
+    props.itemValue.map(() => ({task: "", date: "", time: "" }))
+  );
 
 
- const handleChange = (e) => {
+ const handleChange = (ind,e) => {
     const {name,value} = e.target;
-    setSelectedDateAndTime((prevState)=>({
-      ...prevState,
-      [name]: value
-    }));
+    setSelectedDateAndTime((prevState)=>{
+        const newState = [...prevState];
+        newState[ind] = {...newState[ind],[name]:value};  
+        return newState;
+    });
   };
 
+const onClickCard = ((item,ind)=>{
+    const task = item;    setSelectedDateAndTime((prevState) => {
+      const newState = [...prevState];
+      newState[ind] = { ...newState[ind], task };
+      return newState;
+    });
+    setData(selectedDateAndTime[ind]);
+});
+
+const deleteItem = (ind) => {
+  setSelectedDateAndTime((prevState) => prevState.filter((_, index) => index !== ind));
+  props.setItemValue((prevItems) => prevItems.filter((_, index) => index !== ind));
+};
 
   let item = props.itemValue.map((item, ind) => {
     return (
       
-      <div className="mt10 gridCl" key={Math.random()}>
+      <div className="mt10 gridCl" key={ind}>
        
-        <div className="m5" onClick={setData(selectedDateAndTime)}>
+        <div className="m5" onClick={()=>onClickCard(item,ind)}>
           <div className="mr10 width100cl mb10">
-            <p>{ind+1}. {item.length>10?(item.slice(0,30)+"..."):item}</p>
+          <p>{ind+1}.  {item.length>10?(item.slice(0,30)+"..."):item}</p>
           </div>
-          <input className="mr10 width100cl" type="date" name="date" value={selectedDateAndTime.date} onChange={handleChange}></input>
-          <input className="width100cl" type="time" name="time" value={selectedDateAndTime.time} onChange={handleChange}></input>
-          <Button color="error" variant="contained" className="deleteBtnCl">Delete</Button>
+          <input className="mr10 width100cl" type="date" name="date" value={selectedDateAndTime[ind]?.date || ""} onChange={(e)=>handleChange(ind,e)}></input>
+          <input className="width100cl" type="time" name="time" value={selectedDateAndTime[ind]?.time || ""} onChange={(e)=>handleChange(ind,e)}></input>
+          <Button color="error" variant="contained" className="deleteBtnCl" onClick={()=>deleteItem(ind)}>Delete</Button>
         </div>
       </div>
     );
--- Projects/To_Do_List/src/RightSide/Details/Details.jsx
+++ Projects/To_Do_List/src/RightSide/Details/Details.jsx
@@ -1,21 +1,48 @@
 import React, { useContext } from 'react';
 import { MyContext } from '../../MyProvider';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 function Details() {
     const { data } = useContext(MyContext);
+    console.log(data);
     let time;
     let date;
+    let title;
     for(let item in data) {
+        if(item=="task") {
+            title = data[item];
+        }
+        if(data[item]) {
         if(data[item].includes(":")) {
             time = data[item];
         } else {
             date = data[item];
         }
     }
+    }
 
 return (<>
-          <div> date : {date}</div>
-           <div> time: {time}</div>
+           <div style={{marginTop:'100px'}}>
+           <Card sx={{ minWidth: 450}}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+            <div style={{width:"50px"}}>{title}</div>
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          On {date}
+        </Typography>
+        <Typography variant="body2">
+        At {time}
+        </Typography>
+      </CardContent>
+      
+    </Card>
+    </div>
         </>
     );
 }
